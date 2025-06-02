d264103cdda0110d3e29a7478654eebc312d8df0
Sajid-developer <msasajid01.pro@gmail.com>
2025-06-02 21:37:01 +0530

    files added.

---
+++ script.js
@@ -0,0 +1,57 @@
+// clinic info... ✅
+
+const clinicInfo=[
+    {
+        id:1,
+        image:"Images/3_Booking_Appointment.png",
+        name:"Dr. Zaynab Higher Clinic",
+        address:"New hargeisa udhaw saldhiga new hargeisa"
+    },
+    {
+        id:2,
+        image:"Images/4_ONLINE_PAYMENT.png",
+        name:"Hargeisa obstetrics & Gynecology Hospital",
+        address:"Hargeisa, Galbeedka sariibada lnji"
+    },{
+        id:3,
+        image:"Images/5_Doctor_Consultation.png",
+        name:"KAAH",
+        address:"Hargeisa, 26 june Gol-Janno street"
+    },{
+        id:4,
+        image:"Images/6_E_Prescription.png",
+        name:"Dr. Ismail speciality Hospital",
+        address:"M.Mooge"
+    }
+];
+
+const cardContainer=document.querySelector(".bottomSect .cardContainer");
+
+//  clinicInfo.forEach((clinic)=>{
+//       const {image,name,address}=clinic;
+//       cardContainer.innerHTML+=`
+//       <div class="card h-auto py-5 px-8 flex justify-between items-center">
+//                <div class="left w-auto flex">
+//                 <img src="Images/no-image-found.png" alt="Images">
+//                 <div class="content w-80 flex flex-col justify-between">
+//                   <p class="flex">Doctor Name: <span> Dr Aziza Ibrahim Qabile</span></p>
+//                   <p class="flex">Case ID <span> 2677</span></p>
+//                   <p class="flex">Consultation Type <span> Video</span></p>
+//                   <p class="flex">Consultation Date <span>2023-08-23 01:13 PM</span></p>
+//                 </div>
+//                </div>
+
+//                <div class="right w-auto flex flex-col items-end">
+//                 <h3 class="mt-4">Free</h3>
+//                 <p class="my-1">2024-03-15</p>
+//                 <p class="my-1">17:15 To 19:00</p>
+//                 <div class="buttons flex my-3 ml-4">
+//                   <button class="button"></button>
+//                   <button class="button ml-2"></button>
+//                   <button class="button ml-2"></button>
+//                 </div>
+//                </div>
+//             </div>
+//       `
+// });
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily :{
+        'Montserrat': ['Montserrat','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
