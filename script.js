7b5dc307d18117ff5e61ada769d889043d98cceb
Sajid-developer <msasajid01.pro@gmail.com>
2025-06-03 15:08:42 +0530

    js file modified.

--- script.js
+++ script.js
@@ -55,3 +55,59 @@ const cardContainer=document.querySelector(".bottomSect .cardContainer");
 //       `
 // });
 
+//  clinicInfo.forEach((clinic)=>{
+//       const {image,name,address}=clinic;
+//       cardContainer.innerHTML+=`
+//       <div class="card h-auto py-5 px-8 flex justify-between items-center">
+//                <div class="left w-auto flex">
+//                 <img src="Images/no-image-found.png" alt="Images">
+//                 <div class="content w-80 flex flex-col justify-between">
+//                   <p class="flex">Doctor Name: <span> Dr Aziza Ibrahim Qabile</span></p>
+//                   <p class="flex">Case ID <span> 2677</span></p>
+//                   <p class="flex">Consultation Type <span> Video</span></p>
+//                   <p class="flex">Consultation Date <span>2023-08-23 01:13 PM</span></p>
+//                 </div>
+//                </div>
+
+//                <div class="right w-auto flex flex-col items-end">
+//                 <h3 class="mt-4">Free</h3>
+//                 <p class="my-1">2024-03-15</p>
+//                 <p class="my-1">17:15 To 19:00</p>
+//                 <div class="buttons flex my-3 ml-4">
+//                   <button class="button"></button>
+//                   <button class="button ml-2"></button>
+//                   <button class="button ml-2"></button>
+//                 </div>
+//                </div>
+//             </div>
+//       `
+// });
+
+//  clinicInfo.forEach((clinic)=>{
+//       const {image,name,address}=clinic;
+//       cardContainer.innerHTML+=`
+//       <div class="card h-auto py-5 px-8 flex justify-between items-center">
+//                <div class="left w-auto flex">
+//                 <img src="Images/no-image-found.png" alt="Images">
+//                 <div class="content w-80 flex flex-col justify-between">
+//                   <p class="flex">Doctor Name: <span> Dr Aziza Ibrahim Qabile</span></p>
+//                   <p class="flex">Case ID <span> 2677</span></p>
+//                   <p class="flex">Consultation Type <span> Video</span></p>
+//                   <p class="flex">Consultation Date <span>2023-08-23 01:13 PM</span></p>
+//                 </div>
+//                </div>
+
+//                <div class="right w-auto flex flex-col items-end">
+//                 <h3 class="mt-4">Free</h3>
+//                 <p class="my-1">2024-03-15</p>
+//                 <p class="my-1">17:15 To 19:00</p>
+//                 <div class="buttons flex my-3 ml-4">
+//                   <button class="button"></button>
+//                   <button class="button ml-2"></button>
+//                   <button class="button ml-2"></button>
+//                 </div>
+//                </div>
+//             </div>
+//       `
+// });
+
